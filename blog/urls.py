2b4ee2c3e33a62cbb6fcc86c3dd5443e7e2dc2a3
Dhruvesh48 <103595215+Dhruvesh48@users.noreply.github.com>
2024-09-12 18:34:17 +0000

    added edit and delete option for comments

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path('<slug:slug>/delete_comment/<int:comment_id>',views.comment_delete, name='comment_delete'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',views.comment_edit, name='comment_edit'),
     path('community/<str:name>/', views.community_detail, name='community_detail'),
     path('create-community/', views.create_community, name='create_community'),
     path('create_post/', views.create_post, name='create_post'),
